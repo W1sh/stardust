cc4cce49ea3d22b7b0542a6082a11c90c590e42c
W1sh <brunoferreira.ebm@gmail.com>
2022-02-23 23:06:04 +0000

    Add initial implementation of singletons in functional approach

--- src/test/java/com/w1sh/wave/example/service/impl/CalculatorServiceImpl.java
+++ src/test/java/com/w1sh/wave/example/service/impl/CalculatorServiceImpl.java
@@ -1,7 +1,9 @@
 package com.w1sh.wave.example.service.impl;
 
+import com.w1sh.wave.core.annotation.Inject;
 import com.w1sh.wave.example.service.CalculatorService;
 
 public class CalculatorServiceImpl implements CalculatorService {
+    @Inject
     public CalculatorServiceImpl() { }
 }

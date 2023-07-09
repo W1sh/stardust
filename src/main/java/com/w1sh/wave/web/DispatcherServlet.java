6239635b16eac2ea1de5b9abed9d128f9ed16646
W1sh <brunoferreira.ebm@gmail.com>
2023-07-09 16:17:40 +0100

    Add automatic endpoint discovery using context

--- src/main/java/com/w1sh/wave/web/DispatcherServlet.java
+++ src/main/java/com/w1sh/wave/web/DispatcherServlet.java
@@ -10,22 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.EnumMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final String SEC_WEBSOCKET_KEY = "Sec-WebSocket-Key";
     private final EnumMap<HttpMethod, Set<Route>> routes = new EnumMap<>(HttpMethod.class);
-
-    public void addRoute(Route route) {
-        var existingMethodRoutes = routes.get(route.method());
-        if (existingMethodRoutes == null) existingMethodRoutes = new HashSet<>();
-        existingMethodRoutes.add(route);
-        routes.put(route.method(), existingMethodRoutes);
-    }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -47,5 +39,16 @@ public class DispatcherServlet extends HttpServlet {
                 .filter(route -> path.equalsIgnoreCase(route.path()))
                 .findFirst()
                 .orElseThrow(() -> new RouteMatchingException(String.format("No matching path found for method %s and path %s", method, path)));
+    }
+
+    public void addRoute(Route route) {
+        var existingMethodRoutes = routes.get(route.method());
+        if (existingMethodRoutes == null) existingMethodRoutes = new HashSet<>();
+        existingMethodRoutes.add(route);
+        routes.put(route.method(), existingMethodRoutes);
+    }
+
+    public List<Route> getAllRoutes() {
+        return routes.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
     }
 }

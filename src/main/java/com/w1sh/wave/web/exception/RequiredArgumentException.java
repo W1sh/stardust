b8a3563d8f81d01554b8318d504af55e85b822c3
W1sh <brunoferreira.ebm@gmail.com>
2023-07-09 16:30:01 +0100

    Add QueryParam annotation for better control of parameters

---
+++ src/main/java/com/w1sh/wave/web/exception/RequiredArgumentException.java
@@ -0,0 +1,7 @@
+package com.w1sh.wave.web.exception;
+
+public class RequiredArgumentException extends Exception {
+    public RequiredArgumentException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/w1sh/wave/web/http/QueryParameterMethodArgumentResolver.java
+++ src/main/java/com/w1sh/wave/web/http/QueryParameterMethodArgumentResolver.java
@@ -1,5 +1,7 @@
 package com.w1sh.wave.web.http;
 
+import com.w1sh.wave.web.annotation.QueryParam;
+import com.w1sh.wave.web.exception.RequiredArgumentException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +15,7 @@ public class QueryParameterMethodArgumentResolver {
 
     private static final Logger logger = LoggerFactory.getLogger(QueryParameterMethodArgumentResolver.class);
 
-    public List<MethodArgument> resolveQueryParameters(HttpServletRequest request, Method method) {
+    public List<MethodArgument> resolveQueryParameters(HttpServletRequest request, Method method) throws RequiredArgumentException {
         final var parameterMap = request.getParameterMap();
         final var methodParameters = method.getParameters();
         final var methodArguments = new ArrayList<MethodArgument>();
@@ -24,6 +26,10 @@ public class QueryParameterMethodArgumentResolver {
                 final var value = parameterMap.get(methodParameter.getName());
                 methodArguments.add(new MethodArgument(method, i, value, methodParameter.getType()));
             } else {
+                QueryParam annotation = methodParameter.getAnnotation(QueryParam.class);
+                if (annotation != null && annotation.required()) {
+                    throw new RequiredArgumentException(String.format("Parameter %s is required but it is not present", methodParameter.getName()));
+                }
                 logger.warn("No value found for method parameter {}", methodParameter.getName());
                 methodArguments.add(new MethodArgument(method, i, null, null));
             }
